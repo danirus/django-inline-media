f1e0d4eb8e96318d87cf4ccd949219dc06080c13
Daniel Rus Morales <danirus@eml.cc>
2012-04-11 02:26:43 +0200

    get_version() following PEP 386

---
+++ inline_media/__init__.py
@@ -0,0 +1,12 @@
+"""
+django_inline_media - Simple Django app that allows insertion of inline media objects in text fields.
+"""
+VERSION = (1, 0, 0, 'a', 1) # following PEP 386
+
+def get_version():
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    if VERSION[3] != 'f':
+        version = '%s%s%s' % (version, VERSION[3], VERSION[4])
+    return version
