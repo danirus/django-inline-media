f97f387aa88485b3630f395bcec6ed9be56b1554
Daniel Rus Morales <danirus@eml.cc>
2015-09-02 12:16:39 +0200

    Add runtest.py to test besides tox

---
+++ runtests.py
@@ -0,0 +1,30 @@
+import os
+import sys
+
+def setup_django_settings():
+    sys.path.insert(0, os.getcwd())
+    os.environ["DJANGO_SETTINGS_MODULE"] = "inline_media.tests.settings"
+
+
+def run_tests():
+    if not os.environ.get("DJANGO_SETTINGS_MODULE", False):
+        setup_django_settings()
+
+    import django
+    from django.conf import settings
+    from django.test.utils import get_runner
+    from inline_media.tests import delete_tmp_dirs
+
+    if django.VERSION[1] >= 7: # Django 1.7.x or above
+        django.setup()
+        runner = get_runner(settings,"django.test.runner.DiscoverRunner")
+    else:
+        runner = get_runner(settings,"django.test.simple.DjangoTestSuiteRunner")
+    test_suite = runner(verbosity=2, interactive=True, failfast=False)
+    results = test_suite.run_tests(["inline_media"])
+    delete_tmp_dirs()
+    return results
+
+
+if __name__ == "__main__":
+    run_tests()
